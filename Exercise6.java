fe9116cb58f777fb746720473a6a0948170c0af5
unknown <definevictory111@gmail.com>
2019-03-30 21:29:23 +0700

    Fixed feeback for exercise6

--- Exercise6.java
+++ Exercise6.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
 
 public class Exercise6{
+
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         String str= scan.nextLine();
         swapStringLowerUpper(str);
     }
+
     public static void swapStringLowerUpper(String str){
-        String temp="";
-        for(int i=0;i<str.length();i++)
-        {
-            
+        String temp=""; 
+        for(int i=0;i<str.length();i++){
             temp+=swapCharLowerUpper(str.charAt(i));
         }
         System.out.println(temp);
     }
+
     public static char swapCharLowerUpper(char c){
-        if(Character.isLowerCase(c)){
-            c=Character.toUpperCase(c);
-        }
-        else{
-            c = Character.toLowerCase(c);
-        }
-        return c;
+        return Character.isLowerCase(c) ?Character.toUpperCase(c):Character.toLowerCase(c);
     }
         
 }
