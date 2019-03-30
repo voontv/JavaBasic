e9478ac6f27d911807f7d923f63f65227ecff1e1
unknown <definevictory111@gmail.com>
2019-03-30 21:08:52 +0700

    Fixed feedback for exercise7, about space

--- Exercise7.java
+++ Exercise7.java
@@ -1,12 +1,13 @@
 import java.util.*;
+
 public class Exercise7{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         String superStr = scan.nextLine();
         String childStr = scan.nextLine();
         positionStringChildren(superStr, childStr);
-
     }
+
     public static void positionStringChildren(String superStr,String childStr){
         for(int i=0;i<superStr.length()-childStr.length();i++){
             if(superStr.substring(i, i+childStr.length()).equalsIgnoreCase(childStr)){
