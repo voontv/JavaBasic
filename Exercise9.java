e38456f708883ff450e40206ae5f08a2f056b076
unknown <definevictory111@gmail.com>
2019-03-31 07:14:37 +0700

    Completed feedback for exercise9

--- Exercise9.java
+++ Exercise9.java
@@ -1,38 +1,34 @@
 import java.util.*;
+
 public class Exercise9{
+    
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         String str = scan.nextLine();
         shoortcutString(str);
-
     }
+
     public static void shoortcutString(String str){
         char[] array = new char[str.length()];
-        String tempStr="";
-        int count=1;
+        StringBuffer tempStr=new StringBuffer("");
         array = str.toCharArray();
-        for(int i=0;i<array.length-1;i++){
-            for(int j=i+1;j<array.length;j++){
-                if(array[i]==array[j]){
-                    count++;
-                    if(j==array.length-1){
-                        tempStr = tempStr.concat(array[i]+"");
-                        tempStr = tempStr.concat(count+"");
-                        i=j-1;
-                        break;
-                    }
-                }
-                else{
-                    tempStr = tempStr.concat(array[i]+"");
-                    if(count>1){
-                        tempStr = tempStr.concat(count+"");
-                        count=1;
-                        i=j-1;
-                    }
+
+        for(int i=0; i<array.length-1; i++){
+            int count=1;
+            for(int j=i+1; j<=array.length; j++){
+                if(j==array.length || array[i]!=array[j]){
+                    tempStr = tempStr.append(array[i]);
+                    i=j-1;
                     break;
                 }
+                else{
+                    count++;
+                }
+            }
+            if(count>1){
+                tempStr = tempStr.append(count);
             }
         }
         System.out.println("String after shortcut is "+tempStr);
-    }
+    } 
 }
