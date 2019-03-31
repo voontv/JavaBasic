73cc07cf81d44178593dd48588a3839b6941b9da
unknown <definevictory111@gmail.com>
2019-03-31 10:42:41 +0700

    add space lines

--- Exercise7.java
+++ Exercise7.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Exercise7{
+    
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         String superStr = scan.nextLine();
@@ -9,7 +10,7 @@ public class Exercise7{
     }
 
     public static void positionStringChildren(String superStr,String childStr){
-        
+
         for(int i=0; i<superStr.length()-childStr.length(); i++){
             if(superStr.substring(i, i+childStr.length()).equalsIgnoreCase(childStr)){
                 System.out.println("Position of String Children is "+i);
