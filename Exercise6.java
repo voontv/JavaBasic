599c2b13267fe55b27704083a291be57820d22c2
unknown <definevictory111@gmail.com>
2019-03-31 15:58:22 +0700

    commit exercise 6 again

--- Exercise6.java
+++ Exercise6.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Exercise6{
 
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        String str= scan.nextLine();
-        swapStringLowerUpper(str);
+        Scanner scanner = new Scanner(System.in);
+        String string= scanner.nextLine();
+        swapStringLowerUpper(string);
     }
 
     public static void swapStringLowerUpper(String str){
-        String temp=""; 
-        for(int i=0;i<str.length();i++){
+        String temp="";
+
+        for(int i=0; i<str.length(); i++){
             temp+=swapCharLowerUpper(str.charAt(i));
         }
         System.out.println(temp);
@@ -18,6 +19,5 @@ public class Exercise6{
 
     public static char swapCharLowerUpper(char c){
         return Character.isLowerCase(c) ?Character.toUpperCase(c):Character.toLowerCase(c);
-    }
-        
+    }   
 }
