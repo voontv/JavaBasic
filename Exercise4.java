b18cd2cc8348b83a12fa163894c1b1f577c92d48
unknown <definevictory111@gmail.com>
2019-03-31 14:47:06 +0700

    commit exercise 4

--- Exercise4.java
+++ Exercise4.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Exercise4 {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String str = scan.nextLine();
-		printSumInteger(str);
+		Scanner scanner = new Scanner(System.in);
+		String string = scanner.nextLine();
+		printSumInteger(string);
 	}
 
 	public static void printSumInteger(String str) {
@@ -30,13 +30,15 @@ public class Exercise4 {
 		}
 		return true;
 	}
+
 	public static String removeSpecialCharacter(String str){
-        char lastIndex = str.charAt(str.length()-1);
+		char lastIndex = str.charAt(str.length()-1);
 		if(lastIndex == '?' || lastIndex == '.' || lastIndex == '!' || lastIndex == ','){
 			return str.substring(0,str.length()-1);
 		}
-        return str;
-    }
+        	return str;
+	}
+	   
 	public static boolean isEmpty(String str){
 		return str.length()==0;
 	}
