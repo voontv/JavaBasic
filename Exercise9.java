42e748e70c50d04082f1e6f1f5384a7be5f3361d
unknown <definevictory111@gmail.com>
2019-03-31 11:54:33 +0700

    edit create String

--- Exercise9.java
+++ Exercise9.java
@@ -9,14 +9,14 @@ public class Exercise9{
     }
 
     public static void shortcutString(String str){
-        StringBuffer tempStr=new StringBuffer();
+        StringBuffer buffer=new StringBuffer();
         char[] array = str.toCharArray();
 
         for(int i=0; i<array.length-1; i++){
             int count=1;
             for(int j=i+1; j<=array.length; j++){
                 if(j==array.length || array[i]!=array[j]){
-                    tempStr = tempStr.append(array[i]);
+                    buffer = buffer.append(array[i]);
                     i=j-1;
                     break;
                 }
@@ -25,9 +25,9 @@ public class Exercise9{
                 }
             }
             if(count>1){
-                tempStr = tempStr.append(count);
+                buffer = buffer.append(count);
             }
         }
-        System.out.println("String after shortcut is "+tempStr);
+        System.out.println("String after shortcut is "+buffer);
     } 
 }
