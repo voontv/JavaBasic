b3bd42c4b312cf3207b7a916a7a6731082931309
unknown <definevictory111@gmail.com>
2019-03-31 14:20:47 +0700

    code exercise again

--- Exercise10.java
+++ Exercise10.java
@@ -3,39 +3,32 @@ import java.util.*;
 public class Exercise10{
 
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
         convertString(str);
     }
 
     public static void convertString(String str){
-        char[] array;
-        StringBuffer tempStr = new StringBuffer();
-        array = str.toCharArray();
+        StringBuffer buffer = new StringBuffer();
+        char[] array = str.toCharArray();
 
         for(int i=0; i<array.length; i++){
-            if(Character.isDigit(array[i])){
-                String number="";
-                char c = array[i-1];
-
-                for(int j=i; j<array.length; j++){
-                    if(Character.isDigit(array[j])){
-                        number+=array[j];
-                    }
-                    else{
-                        i=j;
-                        break;
-                    }
-                }
-
-                for(int j=1; j<Integer.parseInt(number); j++){
-                    tempStr = tempStr.append(c);
-                }
+            int count = 0;
+            int j=i+1;
+        
+            // Tìm count 
+            while(j<array.length && Character.isDigit(array[j])){
+                 count = 10*count + array[j] - '0';
+                 j ++;
             }
-            else{
-                tempStr = tempStr.append(array[i]);
+            count = count == 0? 1: count;
+        
+            //Thêm vào count ký tự
+            for(int k=0; k<count; k++){
+                buffer = buffer.append(array[i]);
             }
+            i = j-1;
         }
-        System.out.println("String  after shortcut is "+tempStr);
+        System.out.println("String  after shortcut is "+buffer);
     }
 }
